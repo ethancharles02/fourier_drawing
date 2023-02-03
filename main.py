715591ab2745a1870760639c605044901711eb96
ethancharles02 <ethan.charles02@gmail.com>
2023-02-03 11:39:54 -0800

    Make drawing continuous with points Bug fix line not updating once it hits point limit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 # Try moving the rendering to a different module (manim)
 # Test modulus for degree count
 # too many lines can't be rendered?
+# Don't render circle if it has a radius that is too small
+# Don't update circle if it doesn't ever change
+# Since everything is on a cycle, have it update from an actual time instead of dtime
 from src.renderer import Renderer
 from src.renderer import Window
 from src.arrow_circle_handler import ArrowCircleHandler
@@ -10,11 +13,13 @@ from src.arrow_circle_handler import ArrowCircleHandler
 import numpy
 def drawing_func(x):
     # return numpy.array([complex(-300*num, 0) for num in x])
-    # points = (0 + 100j, 60 + 80j, 50 - 60j, -50 - 60j, -60 + 80j)
-    points = (150 + 100j, 150 - 100j, -50 - 100j, -50 + 100j)
+    # points = numpy.array((0 + 100j, 60 + 80j, 50 - 60j, -50 - 60j, -60 + 80j))
+    points = numpy.array((150 + 100j, 150 - 100j, -50 - 100j, -50 + 100j))
     num_points = len(points)
-    selection = numpy.floor(x * num_points)
-    return numpy.array([points[int(num)] for num in selection])
+    point_multiplier = x * num_points
+    remainder = (point_multiplier) % 1
+    selection = (numpy.floor(point_multiplier).astype(int) + 1) % num_points
+    return (points[selection] - points[selection - 1]) * remainder + points[selection - 1]
     # return numpy.exp(2*numpy.pi*1j*x) * 100 + 100
     # return x*100 + (numpy.array([complex(0, num**2) for num in x]))*100
 
@@ -26,7 +31,7 @@ def main():
     window = Window(renderer=renderer, canvas_width=800, canvas_height=600, update_delay_ms=1)
     arrow_circle_handler = ArrowCircleHandler(
         drawing_func=drawing_func, renderer=window.renderer, arrow_circle_x=middle_x, arrow_circle_y=middle_y,
-        speed_multiplier=1/10, num_arrow_circle_pairs=20, line_duration=0.5, line_update_time=0.01)
+        speed_multiplier=1/10, num_arrow_circle_pairs=3, line_duration=5, line_update_time=0.01)
     window.update_func = arrow_circle_handler.update
     window.mainloop()
 
--- src/resultant_line.py
+++ src/resultant_line.py
@@ -4,7 +4,7 @@ from .arrow_circle import ArrowCircle
 class ResultantLine:
     def __init__(self, tracking_arrow_circle: ArrowCircle, max_line_points: int, line_id: int = None, line_points: list[int] = []):
         self.tracking_arrow_circle = tracking_arrow_circle
-        self.max_line_points = max_line_points
+        self.max_line_points = int(max_line_points + (4 - max_line_points % 4))
         self.line_id = line_id
         self.line_points = line_points
     
@@ -12,8 +12,8 @@ class ResultantLine:
         self.line_points.append(self.tracking_arrow_circle.arrow_x1)
         self.line_points.append(self.tracking_arrow_circle.arrow_y1)
         if len(self.line_points) >= self.max_line_points:
-            self.line_points.pop(0)
-            self.line_points.pop(0)
+            for _ in range(4):
+                self.line_points.pop(0)
     
     def has_even_number_points(self):
         return len(self.line_points) % 4 == 0
